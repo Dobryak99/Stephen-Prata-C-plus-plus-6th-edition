e1cf93d6148b6454935cc4529f822b28d1cf2622
Nikita Zhuravlev <102042747+Dobryak99@users.noreply.github.com>
2023-02-13 22:05:18 +0300

    Create 6.11.cpp

---
+++ Chapter 6/Lectures/6.11.cpp
@@ -0,0 +1,35 @@
+// enum.cpp -- использование enum
+#include <iostream>
+// создаем именованный констант для значений 0 - 6
+enum{red,orange, yellow, green, blue, violet, indigo};
+int main()
+{
+    using namespace std;
+    cout << "Enter color code(0 - 6): ";  //ввод кода цвета
+    int code;
+    cin >> code;
+    while (code >= red && code <= indigo)
+    {
+        switch(code)
+        {
+            case red: cout << "her lips were red.\n";
+                break;
+            case orange: cout << "Her hair was orange.\n";
+                break;
+            case yellow: cout << "Her shoes were yellow";
+                break;
+            case green: cout << "Her nails were green.\n";
+                break;
+            case blue: cout << "Her sweatsuit was blue.\n";
+                break;
+            case violet: cout << "Her eyes were violet.\n";
+                break;
+            case indigo: cout << "Her mood was indigo.\n";
+                break;
+        }
+        cout << "Enter color code(0 - 6): ";
+        cin >> code;
+    }
+    cout << "Bye!\n";
+    return 0;
+}
