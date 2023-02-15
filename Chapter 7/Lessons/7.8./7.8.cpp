761d5fce0e1b63ec27665a72ba361ffabbe41bdd
Nikita Zhuravlev <102042747+Dobryak99@users.noreply.github.com>
2023-02-15 20:23:15 +0300

    Create 7.8.cpp

---
+++ Chapter 7/Lessons/7.8./7.8.cpp
@@ -0,0 +1,26 @@
+// arrfun4.cpp -- функция с диапазоном массива
+#include <iostream>
+const int ArSize = 8;
+int sum_arr(const int * begin, const int * end);
+int main()
+{
+    using namespace std;
+    int cookies[ArSize]= {1,2,4,8,16,32,64,128};
+    int sum = sum_arr(cookies, cookies + ArSize);
+    cout << "Total cookies eaten: " << sum << endl;
+    sum = sum_arr(cookies, cookies + 3);    //три первых элемента
+    cout << "First three eaters ate: " << sum << " cookies.\n";
+    sum = sum_arr(cookies + 4, cookies + 8);   //четыре последних элемента
+    cout << "Last four eaters ate " <<  sum << " cookies.\n";
+    return 0;
+}
+int sum_arr(const int * begin, const int * end)
+{
+    const int * pt;
+    int total = 0;
+    for (pt = begin; pt != end; pt++)
+    {
+        total = total + *pt;
+    }
+    return total;
+}
