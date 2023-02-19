1a1f6f36da9784a1a5f3366d4327cf4452f2358e
Nikita Zhuravlev <102042747+Dobryak99@users.noreply.github.com>
2023-02-19 17:43:44 +0300

    Create namesp.h

---
+++ Chapter 9./Lessons/9.11-13./namesp.h
@@ -0,0 +1,24 @@
+#include <string>
+//  создание пространств имен pers и debts
+namespace pers
+{
+    struct Person
+    {
+        std::string fname;
+        std::string lname;
+    };
+    void getPerson(Person &);
+    void showPerson(const Person &);
+}
+namespace debts
+{
+    using namespace pers;
+    struct Debt
+    {
+        Person name;
+        double amount;
+    };
+    void getDebt(Debt &);
+    void showDebt(const Debt &);
+    double sumDebts(const Debt ar[], int n);
+    }
