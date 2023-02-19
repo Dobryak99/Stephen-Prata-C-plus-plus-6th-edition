39eecd22c18fdcae07763fe169f13a068d3f83a4
Nikita Zhuravlev <102042747+Dobryak99@users.noreply.github.com>
2023-02-19 17:36:57 +0300

    Create 9.4.cpp

---
+++ Chapter 9./Lessons/9.4./9.4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+void oil(int x);
+
+int main()
+{
+    using namespace std;
+    int texas = 31;
+    int year = 2011;
+    cout << "In main(), texas = " << texas << ", &texas = " << &texas << endl;
+    cout << "In main(), year = " << year << ", &year = " << &year << endl;
+    oil(texas);
+    cout << "In main(), texas = " << texas << ", &texas = " << &texas << endl;
+    cout << "In main(), year = " << year << ", &year = " << &year << endl;
+    return 0;
+}
+
+void oil(int x)
+{
+    using namespace std;
+    int texas = 5;
+    cout << "In oil(), texas = " << texas << ", &texas = " << &texas << endl;
+    cout << "In oil(), x = " << x << ", &x = " << &x << endl;
+    {
+        int texas = 113;
+        cout << "In block, texas = " << texas << ", &texas = " << &texas << endl;
+        cout << "In block, x = " << x << ", &x = " << &x << endl;
+    }
+    cout << "Post-block texas = " << texas << ", &texas = " << &texas << endl;
+}
