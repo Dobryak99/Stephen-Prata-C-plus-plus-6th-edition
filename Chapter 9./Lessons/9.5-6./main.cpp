eff459e17a9b29c4448c68fe6c6348776bfdb7ab
Nikita Zhuravlev <102042747+Dobryak99@users.noreply.github.com>
2023-02-19 17:38:01 +0300

    Create main.cpp

---
+++ Chapter 9./Lessons/9.5-6./main.cpp
@@ -0,0 +1,21 @@
+// external.cpp -- внешние переменные
+//компилировать вместе с support.cpp
+#include <iostream>
+
+using namespace std;
+
+double warming = 0.3;  //внешняя переменная
+
+void update(double dt);
+
+void local();
+
+int main()
+{
+    cout << "Global warming is " << warming << " degrees.\n";
+    update(0.1);
+    cout << "Global warming is " << warming << " degrees.\n";
+    local();
+    cout << "Global warming is " << warming << " degrees.\n";
+    return 0;
+}
