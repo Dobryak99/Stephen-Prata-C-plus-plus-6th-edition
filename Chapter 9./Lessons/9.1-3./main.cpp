fd92f0cdd9d14fa0f872d2085e5f0c4673c1cf78
Nikita Zhuravlev <102042747+Dobryak99@users.noreply.github.com>
2023-02-19 17:35:13 +0300

    Create main.cpp

---
+++ Chapter 9./Lessons/9.1-3./main.cpp
@@ -0,0 +1,20 @@
+//file1.cpp -- пример программы, состоящей из трех файлов
+#include <iostream>
+#include "coordin.h"
+
+using namespace std;
+int main()
+{
+    rect rplace;
+    polar pplace;
+    
+    cout << "Enter the x and y values: ";
+    while(cin >> rplace.x >> rplace.y)
+    {
+        pplace = rect_to_polar(rplace);
+        show_polar(pplace);
+        cout << "Next two numbers(q to quit): ";
+    }
+    cout << "Done!\n";
+    return 0;
+}
