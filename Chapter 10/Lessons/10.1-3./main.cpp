ca0dc96d0986271e961b2ae3a6e9d4674abffe50
Nikita Zhuravlev <102042747+Dobryak99@users.noreply.github.com>
2023-02-22 16:39:15 +0300

    Create main.cpp

---
+++ Chapter 10/Lessons/10.1-3./main.cpp
@@ -0,0 +1,22 @@
+//usestock0.cpp -- клиентская программа
+#include <iostream>
+#include "stock.h"
+
+
+int main()
+{
+    Stock fluffy_the_cat;
+    fluffy_the_cat.acquire("namoSmart", 20, 12.50);
+    fluffy_the_cat.show();
+    fluffy_the_cat.buy(15, 18.125);
+    fluffy_the_cat.show();
+    fluffy_the_cat.sell(400, 20.00);
+    fluffy_the_cat.show();
+    fluffy_the_cat.buy(300000, 40.125);
+    fluffy_the_cat.show();
+    fluffy_the_cat.sell(300000, 0.125);
+    fluffy_the_cat.show();
+    return 0;
+}
+
+
