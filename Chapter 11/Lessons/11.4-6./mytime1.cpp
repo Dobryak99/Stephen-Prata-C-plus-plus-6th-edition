3886e4a99a32e8f9f32a236834566926e0b1ed28
Nikita Zhuravlev <102042747+Dobryak99@users.noreply.github.com>
2023-02-26 15:06:15 +0300

    Create mytime1.cpp

---
+++ Chapter 11/Lessons/11.4-6./mytime1.cpp
@@ -0,0 +1,48 @@
+//mytime1.cpp -- реализация методов Time
+#include <iostream>
+#include "mytime1.hpp"
+
+Time::Time()
+{
+    hours = 0;
+    minutes = 0;
+}
+
+Time::Time(int h, int m)
+{
+    hours = h;
+    minutes = m;
+}
+
+void Time::AddMin(int m)
+{
+    minutes += m;
+    hours += minutes / 60;
+    minutes %= 60;
+}
+
+void Time::AddHr(int h)
+{
+    hours += h;
+}
+
+void Time::Reset(int h, int m)
+{
+    hours = h;
+    minutes = m;
+}
+
+Time Time::operator+(const Time & t) const
+{
+    Time sum;
+    sum.minutes = minutes + t.minutes;
+    sum.hours = hours + t.hours + sum.minutes / 60;
+    sum.minutes %= 60;
+    return sum;
+}
+
+void Time::show()const
+{
+    std::cout << hours << " hours, " << minutes << " minutes.\n";
+}
+
