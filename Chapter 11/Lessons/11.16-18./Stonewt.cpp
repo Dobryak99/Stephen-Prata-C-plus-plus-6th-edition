c7a2d4c83f59666fa76660f89b5593385e79254f
Nikita Zhuravlev <102042747+Dobryak99@users.noreply.github.com>
2023-02-26 15:10:41 +0300

    Create Stonewt.cpp

---
+++ Chapter 11/Lessons/11.16-18./Stonewt.cpp
@@ -0,0 +1,40 @@
+//stonewt.cpp -- методы класса stonewt
+#include <iostream>
+#include "Stonewt.hpp"
+using std::cout;
+
+//Конструирует объект Stonewt из значений double
+Stonewt::Stonewt(double lbs)
+{
+    stone = int(lbs) / Lbs_per_stn;     //целочисленное деление
+    pds_left = int(lbs) % Lbs_per_stn + lbs - int(lbs);
+    pounds = lbs;
+}
+
+//Конструктор объекта Stonewt из стоунов и значений типа double
+Stonewt::Stonewt(int stn, double lbs)
+{
+    stone = stn;
+    pds_left = lbs;
+    pounds = stn * Lbs_per_stn + lbs;
+}
+
+Stonewt::Stonewt()
+{
+    stone = pounds = pds_left = 0;
+}
+
+Stonewt::~Stonewt()
+{
+    
+}
+
+void Stonewt::show_stn() const
+{
+    cout << stone << " stone, " << pds_left << " pounds.\n";
+}
+
+void Stonewt::show_lbs() const
+{
+    cout << pounds << " pounds.\n";
+}
