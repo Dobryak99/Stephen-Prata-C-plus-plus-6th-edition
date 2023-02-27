8d59d665504e3188f977a76ed9ea4621ae6050d1
Nikita Zhuravlev <102042747+Dobryak99@users.noreply.github.com>
2023-02-27 18:31:38 +0300

    Create main.cpp

---
+++ Chapter 11/Exercises/11.2./main.cpp
@@ -0,0 +1,48 @@
+//randwalk.cpp --  использование класса Vector
+#include <iostream>
+#include <cstdlib>   //прототипы rand(), srand()
+#include <ctime>     //прототип time();
+#include "11.2.h"
+#include "11.2.cpp"
+
+int main()
+{
+    using namespace std;
+    using VECTOR::Vector;
+    srand(time(0));   //начальное значение для генератора случайных чисел
+    double direction;
+    Vector step;
+    Vector result(0.0, 0.0);
+    unsigned long steps = 0;
+    double target;
+    double dstep;
+    cout << "Enter  target distance(q to quit):";
+    while(cin >> target)
+    {
+        cout << "Enter step length: ";
+        if(!(cin >> dstep))
+            break;
+        while(result.magval() < target)
+        {
+            direction = rand() % 360;
+            step.reset(dstep, direction, Vector::POL);
+            result = result + step;
+            steps++;
+        }
+        cout << "After " << steps << " steps, the subject "
+        << "has the following location:\n";
+        cout << result << endl;    //вывод позиции steps шагов
+        result.polar_mode();
+        cout << " or\n" << result << endl;
+        cout << "Average outward distance per step = "
+        << result.magval()/ steps << endl;  //выод среднего расстояния на один шаг
+        steps = 0;
+        result.reset(0.0, 0.0);
+        cout << "Enter target distance (q to quit): ";
+    }
+    cout << "Bye!\n";
+    cin.clear();
+    while(cin.get() != '\n')
+        continue;
+    return 0;
+}
