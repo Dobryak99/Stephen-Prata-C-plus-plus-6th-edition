e844c956df3e126f51a3ab9a865b7230a3480cac
Nikita Zhuravlev <102042747+Dobryak99@users.noreply.github.com>
2023-03-02 21:38:00 +0300

    Create main.cpp

---
+++ Chapter 13/Lessons/13.7-9./main.cpp
@@ -0,0 +1,27 @@
+//usebrass.cpp -- тестирование классов банковских счетов
+
+#include "brass.hpp"
+#include <iostream>
+
+int main()
+{
+    using std::cout;
+    using std::endl;
+    
+    Brass Piggy("Porcelot Pigg", 381299, 4000.00);
+    BrassPlus Hoggy("Horatio Hogg", 382288, 3000.00);
+    Piggy.ViewAcct();
+    cout << endl;
+    Hoggy.ViewAcct();
+    cout << endl;
+    cout << "Depositing $1000 into the Hogg account:\n";
+    Hoggy.Deposit(1000.00);
+    cout << "New balance: $" << Hoggy.Balance() << endl;
+    cout << "Withdrawing from the Pigg account: \n";
+    Piggy.Withdraw(4200.00);
+    cout << "Pigg account balance: " << Piggy.Balance() << endl;
+    cout << "Withdrawing $4200 from the Hogg account:\n";
+    Hoggy.Withdraw(4200.00);
+    Hoggy.ViewAcct();
+    return 0;
+}
