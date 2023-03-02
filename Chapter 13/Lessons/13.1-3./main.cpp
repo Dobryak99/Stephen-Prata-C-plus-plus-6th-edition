7ba7bba4698f98885e2f3c9d8c276815c193eb42
Nikita Zhuravlev <102042747+Dobryak99@users.noreply.github.com>
2023-03-02 21:35:07 +0300

    Create main.cpp

---
+++ Chapter 13/Lessons/13.1-3./main.cpp
@@ -0,0 +1,25 @@
+#include "tabtenn.hpp"
+
+#include <iostream>
+
+int main()
+{
+    using std::cout;
+    TableTennisPlayer player1("Chuck", "Blizzard" , true);
+    TableTennisPlayer player2("Tara", "Boomdea" , false);
+    player1.Name();
+    if(player1.HasTable())
+    {
+        cout << ": has a table.\n";
+    }else{
+        cout << ": hasn't a table.\n";
+    }
+    player2.Name();
+    if(player2.HasTable())
+    {
+        cout << ": has a table.\n";
+    }else{
+        cout << ": hasn't a table.\n";
+    }
+    return 0;
+}
