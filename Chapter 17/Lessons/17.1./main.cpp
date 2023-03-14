31b7ae97b9d8d90e9a21a274c9257733138be6d9
Nikita Zhuravlev <102042747+Dobryak99@users.noreply.github.com>
2023-03-14 20:51:49 +0300

    Create main.cpp

---
+++ Chapter 17/Lessons/17.1./main.cpp
@@ -0,0 +1,31 @@
+//write.cpp -- использование cout.write()
+#include <iostream>
+#include <cstring>
+
+int main()
+{
+    using std::cout;
+    using std::endl;
+    
+    const char * state1 = "Florida";
+    const char * state2 = "Kansas";
+    const char * state3 = "Euphoria";
+    int len = std::strlen(state2);
+    cout << "Increasing loop index:\n";
+    int i;
+    for(i = 1; i <= len; i++)
+    {
+        cout.write(state2,i);
+        cout << endl;
+    }
+    //конкатенация вывода
+    cout << "Decreasing loop index:\n";
+    for(i = len; i > 0; i--)
+    {
+        cout.write(state2,i) << endl;
+    }
+    //превышение длины строки
+    cout << "Exceeding string length:\n";
+    cout.write(state2,len + 5) << endl;
+    return 0;
+}
