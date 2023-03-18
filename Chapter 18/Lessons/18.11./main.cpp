9fd684afad84edc6428c24fc8e3c7911f80a7700
Nikita Zhuravlev <102042747+Dobryak99@users.noreply.github.com>
2023-03-18 23:32:46 +0300

    Create main.cpp

---
+++ Chapter 18/Lessons/18.11./main.cpp
@@ -0,0 +1,19 @@
+// lexcast.cpp -- простое преобразование из float в string
+#include <iostream>
+#include <string>
+#include <boost/lexical_cast.hpp>
+
+int main()
+{
+    using namespace std;
+    cout << "Enter your weight: ";
+    float weight;
+    cin >> weight;
+    string gain = "A 10% increase raises ";
+    string wt = boost::lexical_cast<string>(weight);
+    gain = gain + wt + " to ";
+    weight = 1.1 * weight;
+    gain = gain + boost::lexical_cast<string>(weight) + ".";
+    cout << gain << endl;
+    return 0;
+}
