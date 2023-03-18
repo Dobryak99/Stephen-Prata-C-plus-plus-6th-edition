c562d8bafe4406660c717be2ec557e6cffa1d49e
Nikita Zhuravlev <102042747+Dobryak99@users.noreply.github.com>
2023-03-18 23:31:57 +0300

    Create main.cpp

---
+++ Chapter 18/Lessons/18.10./main.cpp
@@ -0,0 +1,31 @@
+// variadic2.cpp -- использование рекурсии для распаковки пакета параметров
+#include <iostream>
+#include <string>
+
+// определение для 0 параметров -- завершение вызова
+void show_list3() {}
+
+//определение для 1 параметра
+template <typename T>
+void show_list3(const T & value)
+{
+    std::cout << value << std::endl;
+}
+
+//определение для более параметров
+template<typename T, typename...Args>
+void show_list3(T value, Args...args)
+{
+    std::cout << value << ", ";
+    show_list3(args...);
+}
+
+int main()
+{
+    int n = 14;
+    double x = 2.71828;
+    std::string mr = "Mr. String object!";
+    show_list3(n,x);
+    show_list3(x*x, '!', 7, mr);
+    return 0;
+}
